589fed3f3f8e4ae5b63f597b045e49af5ba05367
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 15:58:54 -0300

    Mudança do caminho de history

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-import history from './config/history';
+import history from './services/history';
 
 import store from './store';
 
