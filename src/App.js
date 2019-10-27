4f382e7a19f77d0b0a172d3af61fdc06a73b0c4c
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 08:50:44 -0300

    Configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import Routes from './routes';
+import history from './config/history';
 
 function App() {
   return (
-    <>
-      <h1>Meetapp</h1>
+    <Router history={history}>
+      <Routes />
       <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
