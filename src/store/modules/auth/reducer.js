86f789778113a2f3b3395d24c9006ee2fc8434fd
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 17:14:00 -0300

    Autenticação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,10 +1,20 @@
+import produce from 'immer';
+
 const INITIAL_STATE = {
-  token: 123456,
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.token = action.payload.token;
+        draft.signed = true;
+        break;
+      }
+      default:
+    }
+  });
 }
