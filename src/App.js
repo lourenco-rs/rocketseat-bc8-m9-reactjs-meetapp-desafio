6da11402f4368aeee5e5a294ea99be932feedc42
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 08:09:36 -0300

    Estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>Meetapp</h1>;
+  return (
+    <>
+      <h1>Meetapp</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
