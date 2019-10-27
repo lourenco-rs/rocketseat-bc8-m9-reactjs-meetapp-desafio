058f7fc94f10c2aca7fa578de7c4d2642d52a29c
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 14:43:02 -0300

    Validações nas página de autenticação

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as yup from 'yup';
 import { Link } from 'react-router-dom';
 import { Form } from '@rocketseat/unform';
 
@@ -6,12 +7,28 @@ import { Input, Button } from '~/components';
 
 import logo from '~/assets/logo.svg';
 
+const schema = yup.object().shape({
+  name: yup.string().required('O nome é obrigatório'),
+  email: yup
+    .string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: yup
+    .string()
+    .min(6, 'A senha precisa ter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Meetapp" />
 
-      <Form>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input
