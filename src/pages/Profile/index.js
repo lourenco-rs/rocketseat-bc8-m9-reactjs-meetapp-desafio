f25f3440d012ceb8143697dc2d4efa0ceb854968
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-28 05:23:32 -0300

    Salvando alteranções no Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Input } from '~/components';
 
 import { StyledForm, SaveButton, ButtonWrapper } from './styles';
 
 export default function SignIn() {
-  function handleSubmit() {}
+  const dispatch = useDispatch();
+  const { profile } = useSelector(state => state.user);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
-    <StyledForm onSubmit={handleSubmit}>
+    <StyledForm onSubmit={handleSubmit} initialData={profile}>
       <Input name="name" placeholder="Nome completo" />
       <Input name="email" type="email" placeholder="Digite seu e-mail" />
       <hr />
